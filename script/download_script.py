b51dfa725d3689bde3a5bb87c557789d7ad18fd2
ferz <fedotov_clever_91@mail.ru>
2022-10-27 01:37:30 +0300

    Fix download dependencies script

--- script/download_script.py
+++ script/download_script.py
@@ -1,28 +1,29 @@
 import requests
 import zipfile
 import os
+import re
 
 if __name__ == "__main__":
     print("\nDownload dependencies")
-
-    libs_names = {"SFML" : "SFML-2.5.1",
-                  "TGUI" : "TGUI-1.0" }
     
-    libs = {"SFML" : "https://www.sfml-dev.org/files/SFML-2.5.1-windows-vc15-64-bit.zip",
-            "TGUI" : "https://github.com/texus/TGUI/releases/download/v1.0-alpha/TGUI-1.0-alpha-VisualStudio-64bit-for-SFML-2.5.1.zip" }
-
+    libs = { "SFML" : "https://www.sfml-dev.org/files/SFML-2.5.1-windows-vc15-64-bit.zip",
+             "TGUI" : "https://github.com/texus/TGUI/releases/download/v1.0-alpha/TGUI-1.0-alpha-VisualStudio-64bit-for-SFML-2.5.1.zip" }
+    
     try:
         externalPath = "../external"
-        os.mkdir(externalPath)
+        if not os.path.exists(externalPath):
+            os.mkdir(externalPath)
         os.chdir(externalPath)
-
+        
         for name, url in libs.items():
-
-            if os.path.exists("../external/{}".format(name)):
-                print("-> {} already downloaded\n".format(name))
-                continue
             
-            print("-> Downloading {} library from: {}".format(name).format(url))
+            print("-> Downloading {} from: {}".format(name, url))
+            
+            if os.path.exists(externalPath + "/{}".format(name)):
+                print("-> {} already downloaded".format(name))
+                continue
+
+            libRegex = r'^{}[\d\_\-\.]+$'.format(name)
             with open("{}.zip".format(name), "wb") as file:
                 file.write(requests.get(url).content)
 
@@ -30,11 +31,14 @@ if __name__ == "__main__":
             with zipfile.ZipFile("{}.zip".format(name)) as zip:
                 zip.extractall("./")
 
-            os.rename(libs_names[name], name)
+            extractDir = [curDir for curDir in os.listdir() if re.fullmatch(libRegex, curDir)][0]
+            os.rename(extractDir, name)
             os.remove("{}.zip".format(name))
 
-            print("Success with {}".format(name))
+            print("-> Success {}".format(name))
+
+        print("Successful download dependencies")
             
-    except BaseException:
-        print("-> Error was occured\nFail")
+    except BaseException as e:
+        print("-> Error was occured: {}\nFail".format(e))
         exit(0)
