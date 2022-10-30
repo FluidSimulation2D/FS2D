c4cf691608bcb804e5a2cf32cad06d0c76e6197d
FeRZ <fedotov_clever_91@mail.ru>
2022-10-30 23:27:50 +0300

    Add downloading and linking TGUI library

--- script/download_script.py
+++ script/download_script.py
@@ -1,32 +1,41 @@
 import requests
 import zipfile
 import os
+import re
 
 if __name__ == "__main__":
     print("\nDownload dependencies")
 
-    if os.path.exists("../external/SFML"):
-        print("-> SFML already downloaded\nSuccess")
-        exit(0)
+    libs = {"SFML-2.5.1" : "https://www.sfml-dev.org/files/SFML-2.5.1-windows-vc15-64-bit.zip",
+            "TGUI-1.0" : "https://github.com/texus/TGUI/releases/download/v1.0-alpha/TGUI-1.0-alpha-VisualStudio-64bit-for-SFML-2.5.1.zip" }
+    externalPath = "../external"
 
     try:
-        externalPath = "../external"
-        os.mkdir(externalPath)
+        if not os.path.exists(externalPath):
+            os.mkdir(externalPath)
         os.chdir(externalPath)
 
-        URL = "https://www.sfml-dev.org/files/SFML-2.5.1-windows-vc15-64-bit.zip"
-        print("-> Downloading sfml library from: " + URL)
-        with open("sfml.zip", "wb") as file:
-            file.write(requests.get(URL).content)
+        for name, url in libs.items():
 
-        print("-> Extracting files from archive")
-        with zipfile.ZipFile("sfml.zip") as zip:
-            zip.extractall("./")
+            print("-> Downloading {} from: {}".format(name, url))
 
-        os.rename("SFML-2.5.1", "SFML")
-        os.remove("sfml.zip")
+            if os.path.exists("{}/{}".format(externalPath, name)):
+                print("-> {} already downloaded".format(name))
+                continue
 
-        print("Success")
-    except BaseException:
-        print("-> Error was occured\nFail")
+            with open("{}.zip".format(name), "wb") as file:
+                file.write(requests.get(url).content)
+
+            print("-> Extracting files from archive")
+            with zipfile.ZipFile("{}.zip".format(name)) as zip:
+                zip.extractall("./")
+
+            os.remove("{}.zip".format(name))
+
+            print("-> Success {}".format(name))
+
+        print("Successful download dependencies")
+
+    except BaseException as e:
+        print("-> Error was occured: {}\nFail".format(e))
         exit(0)
