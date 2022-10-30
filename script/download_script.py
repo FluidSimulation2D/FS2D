622b1402ff48a3bf2abc5dc2adfeb08d9dca84f2
ferz <fedotov_clever_91@mail.ru>
2022-10-30 21:37:24 +0300

    Small fixes in download dependencies script and CMakeLists

--- script/download_script.py
+++ script/download_script.py
@@ -5,25 +5,24 @@ import re
 
 if __name__ == "__main__":
     print("\nDownload dependencies")
-    
-    libs = { "SFML" : "https://www.sfml-dev.org/files/SFML-2.5.1-windows-vc15-64-bit.zip",
-             "TGUI" : "https://github.com/texus/TGUI/releases/download/v1.0-alpha/TGUI-1.0-alpha-VisualStudio-64bit-for-SFML-2.5.1.zip" }
-    
+
+    libs = {"SFML-2.5.1" : "https://www.sfml-dev.org/files/SFML-2.5.1-windows-vc15-64-bit.zip",
+            "TGUI-1.0" : "https://github.com/texus/TGUI/releases/download/v1.0-alpha/TGUI-1.0-alpha-VisualStudio-64bit-for-SFML-2.5.1.zip" }
+    externalPath = "../external"
+
     try:
-        externalPath = "../external"
         if not os.path.exists(externalPath):
             os.mkdir(externalPath)
         os.chdir(externalPath)
-        
+
         for name, url in libs.items():
-            
+
             print("-> Downloading {} from: {}".format(name, url))
-            
-            if os.path.exists(externalPath + "/{}".format(name)):
+
+            if os.path.exists("{}/{}".format(externalPath, name)):
                 print("-> {} already downloaded".format(name))
                 continue
 
-            libRegex = r'^{}[\d\_\-\.]+$'.format(name)
             with open("{}.zip".format(name), "wb") as file:
                 file.write(requests.get(url).content)
 
@@ -31,14 +30,12 @@ if __name__ == "__main__":
             with zipfile.ZipFile("{}.zip".format(name)) as zip:
                 zip.extractall("./")
 
-            extractDir = [curDir for curDir in os.listdir() if re.fullmatch(libRegex, curDir)][0]
-            os.rename(extractDir, name)
             os.remove("{}.zip".format(name))
 
             print("-> Success {}".format(name))
 
         print("Successful download dependencies")
-            
+
     except BaseException as e:
         print("-> Error was occured: {}\nFail".format(e))
         exit(0)
